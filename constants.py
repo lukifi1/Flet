b684e911f4956be63ad637f901af56b60838b001
Ibrahim Elsherif <32613923+elsherif896@users.noreply.github.com>
2025-12-14 19:23:48 +0100

    Refactor constants.py for improved readability; organize QR code configurations and patterns

--- constants.py
+++ constants.py
@@ -1,14 +1,18 @@
 import base64
-import qrcode
 from enum import Enum
 
+import qrcode
+import qrcode.constants
+
 # Transparent 1x1 PNG image byte data
-TRANSPARENT_PNG=b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x06\x00\x00\x00\x1f\x15\xc4\x89\x00\x00\x00\nIDATx\x9cc``\x00\x00\x00\x02\x00\x01\xe2!\xbc3\x00\x00\x00\x00IEND\xaeB`\x82'
+TRANSPARENT_PNG = b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x06\x00\x00\x00\x1f\x15\xc4\x89\x00\x00\x00\nIDATx\x9cc``\x00\x00\x00\x02\x00\x01\xe2!\xbc3\x00\x00\x00\x00IEND\xaeB`\x82"
 # Base64 encoded transparent PNG
-TRANSPARENT_BASE64_PNG=base64.b64encode(TRANSPARENT_PNG).decode()
+TRANSPARENT_BASE64_PNG = base64.b64encode(TRANSPARENT_PNG).decode()
+
 
 class QRCodeDataType(Enum):
     """Enumeration of QR code data types/formats."""
+
     TEXT = "Text"
     URL = "URL"
     EMAIL = "Email"
@@ -16,6 +20,7 @@ class QRCodeDataType(Enum):
     SMS = "SMS"
     WIFI = "WiFi"
     GEOLOCATION = "Geolocation"
+
 
 # ====================================
 #       Natural Language Patterns
@@ -39,47 +44,54 @@ GEOLOCATION_PATTERN = r"-?\d+(\.\d+)?,-?\d+(\.\d+)?"
 # Mapping of QR code data types to their regex patterns
 TYPE_PATTERNS = {
     # Natural Language Patterns
-    QRCodeDataType.PHONE : PHONE_NUMBER_PATTERN,
-    QRCodeDataType.EMAIL : EMAIL_PATTERN,
-    QRCodeDataType.URL : DOMAIN_PATTERN,
+    QRCodeDataType.PHONE: PHONE_NUMBER_PATTERN,
+    QRCodeDataType.EMAIL: EMAIL_PATTERN,
+    QRCodeDataType.URL: DOMAIN_PATTERN,
     # Additional Patterns
-    QRCodeDataType.SMS : SMS_PATTERN,
-    QRCodeDataType.WIFI : WIFI_PATTERN,
-    QRCodeDataType.GEOLOCATION : GEOLOCATION_PATTERN,
+    QRCodeDataType.SMS: SMS_PATTERN,
+    QRCodeDataType.WIFI: WIFI_PATTERN,
+    QRCodeDataType.GEOLOCATION: GEOLOCATION_PATTERN,
 }
 
 # Mapping of QR code data types to their URI schemes
 URI_SCHEMES = {
-    QRCodeDataType.URL : ["https://", "http://"],
-    QRCodeDataType.EMAIL : "mailto:",
-    QRCodeDataType.PHONE : "tel:",
-    QRCodeDataType.SMS : "sms:",
-    QRCodeDataType.GEOLOCATION : "geo:",
-    QRCodeDataType.WIFI : "WIFI:",
+    QRCodeDataType.URL: ["https://", "http://"],
+    QRCodeDataType.EMAIL: "mailto:",
+    QRCodeDataType.PHONE: "tel:",
+    QRCodeDataType.SMS: "sms:",
+    QRCodeDataType.GEOLOCATION: "geo:",
+    QRCodeDataType.WIFI: "WIFI:",
 }
+
+# ====================================
+#       QR Code ECC Configuration
+# ====================================
+
 
 class QRCodeEccLevel(Enum):
     """Enumeration of QR code error correction levels."""
+
     L = "L"  # Low
     M = "M"  # Medium
     Q = "Q"  # Quartile
     H = "H"  # High
     NA = "N/A"  # Not Applicable
 
+
 # QR Code capacity limits for version 40 and error correction levels
 QR_LIMITS = {
-    QRCodeEccLevel.L: 2953, # Low error correction
-    QRCodeEccLevel.M: 2331, # Medium error correction
-    QRCodeEccLevel.Q: 1663, # Quartile error correction 
-    QRCodeEccLevel.H: 1273, # Highest error correction
+    QRCodeEccLevel.L: 2953,  # Low error correction
+    QRCodeEccLevel.M: 2331,  # Medium error correction
+    QRCodeEccLevel.Q: 1663,  # Quartile error correction
+    QRCodeEccLevel.H: 1273,  # Highest error correction
 }
 # Colors associated with each ECC level
 QR_ECC_LEVEL_COLORS = {
-    QRCodeEccLevel.L: "#dc3545", # Red
-    QRCodeEccLevel.M: "#fd7e14", # Orange
-    QRCodeEccLevel.Q: "#0d6efd", # Blue
-    QRCodeEccLevel.H: "#26C77C", # Green
-    QRCodeEccLevel.NA: "#6c757d", # Grey
+    QRCodeEccLevel.L: "#dc3545",  # Red
+    QRCodeEccLevel.M: "#fd7e14",  # Orange
+    QRCodeEccLevel.Q: "#0d6efd",  # Blue
+    QRCodeEccLevel.H: "#26C77C",  # Green
+    QRCodeEccLevel.NA: "#6c757d",  # Grey
 }
 # Descriptive messages for each ECC level
 QR_ECC_LEVEL_MESSAGES = {
@@ -113,13 +125,23 @@ ECC_ORDER = [
     QRCodeEccLevel.L,
 ]
 
+# ====================================
+#       QR Code Configuration
+# ====================================
+QR_FILL_COLOR = "black"
+QR_BACK_COLOR = "white"
+
+# ====================================
+#       Logo Configuration
+# ====================================
+
 # Path to logo image asset
-LOGO_PATH="assets/logo-color.png"
+LOGO_PATH = "assets/logo-color.png"
 # Logo area as a ratio of QR code size in percentage
-LOGO_AREA_RATIO=0.22
+LOGO_AREA_RATIO = 0.22
 # Logo padding as a ratio of logo size
-LOGO_AREA_PADDING_RATIO=0
+LOGO_AREA_PADDING_RATIO = 0
 # Logo corner radius as a ratio of logo size
-LOGO_AREA_RADIUS_RATIO=0.25
+LOGO_AREA_RADIUS_RATIO = 0.25
 # Minimum ECC level required when adding a logo
-LOGO_MIN_ECC_LEVEL=QRCodeEccLevel.H
+LOGO_MIN_ECC_LEVEL = QRCodeEccLevel.H
