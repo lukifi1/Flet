c953e888c251939ca8d47229a6017d5a943e1e40
Ibrahim Elsherif <32613923+elsherif896@users.noreply.github.com>
2026-05-14 13:02:40 +0200

    move asset folder to src

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import flet as ft
+import logging
 
 from app.app import main
 from app.core.config import LOGGING_LEVEL
@@ -7,4 +8,11 @@ from app.core.logger import setup_logging
 if __name__ == "__main__":
     # Initialize logging
     setup_logging(level=LOGGING_LEVEL)
+    
+    # Get a logger and log startup
+    log = logging.getLogger("qr_maker")
+    log.info("════════════════════════════════════════════")
+    log.info("APPLICATION STARTED")
+    log.info("════════════════════════════════════════════")
+    
     ft.run(main, assets_dir="assets")
